f76fabe371177f7b2d588e7a1c5226db9db54d53
Oleg Sh <oleg.sh.dev@gmail.com>
2017-06-01 16:50:50 +0300

    PHP#7 Fix failure of Tests\AppBundle\Controller\Api\UserControllerTest::testSetRoles403 (User can only change it's own role)

--- symfony_demo/src/AppBundle/Controller/Api/UserController.php
+++ symfony_demo/src/AppBundle/Controller/Api/UserController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Api;
 use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as FOS;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class UserController extends Controller
 {
@@ -39,6 +40,10 @@ class UserController extends Controller
      */
     public function setRolesAction(User $theUser, array $roles)
     {
+        if ($this->getUser() != $theUser) {
+            throw new AccessDeniedException;
+        }
+
         $theUser->setRoles($roles);
 
         $manager = $this->getDoctrine()->getManagerForClass(User::class);
