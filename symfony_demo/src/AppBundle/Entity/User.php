d36775dd71b4481e1411d783351631737ac379c1
Anton Korniychuk <dev@korniychuk.pro>
2017-06-24 02:32:17 +0300

    PHP#3 Rename the "@JMS*" annotation from the src/AppBundle/Entity folder to "@Serializer*" - done

--- symfony_demo/src/AppBundle/Entity/User.php
+++ symfony_demo/src/AppBundle/Entity/User.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use JMS\Serializer\Annotation as JMS;
+use JMS\Serializer\Annotation as Serializer;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
@@ -48,7 +48,7 @@ class User implements UserInterface
      * @var string
      *
      * @ORM\Column(type="string")
-     * @JMS\Exclude()
+     * @Serializer\Exclude()
      */
     private $password;
 
@@ -56,7 +56,7 @@ class User implements UserInterface
      * @var array
      *
      * @ORM\Column(type="json_array")
-     * @JMS\Accessor(getter="getRoles")
+     * @Serializer\Accessor(getter="getRoles")
      */
     private $roles = [];
 
